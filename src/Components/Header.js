f6e23aa78047538d02e8f9628665b996ac737516
Lakshya Kumar <135580752+its-lakshya@users.noreply.github.com>
2023-07-22 01:00:27 +0530

    Styled the Responsive Header buttons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,8 @@ import { tagSearch } from "../Utils/tagSearchSlice";
 import { options } from "../Constants";
 import { search } from "../Utils/searchSlice";
 import { searchDetails } from "../Utils/detailsSlice";
+import { MdArrowDropDown, MdArrowDropUp } from "react-icons/md";
+
 
 const Header = () => {
   const navigate = useNavigate();
@@ -56,11 +58,13 @@ const Header = () => {
     <div className="fixed top-0 bg-opacity-95 bg-black h-16 w-full z-10 flex flex-col max-lg:flex-shrink">
       <div className="flex justify-between mx-28 pt-2 max-xl:mx-20 max-lg:mx-16 max-md:mx-12  max-sm:mx-4 items-start">
         <Link to="/" className="flex text-red-700 items-center cursor-pointer font-semibold text-2xl max-sm:text-xl mt-[0.5rem] max-sm:mt-[0.6rem] ">MoviesTrip</Link>
-        <div className="cursor-pointer ml-4 max-sm:ml-2 mt-3 max-sm:mt-[0.75rem]   
-         font-light text-lg max-sm:text-base invisible max-md:visible relative" onClick={flagHandler}>Menu
-         {flag && (<div className=" flex-col justify-evenly [&>*]:flex  mt-3  
-        items-center  [&>span]:cursor-pointer font-light text-lg ">
-          <Link to="/" className="hover:text-red-600 hover:font-normal">
+        <div className="cursor-pointer ml-4 max-sm:ml-2 mt-3 max-sm:mt-[0.75rem] 
+         font-light text-lg max-sm:text-base invisible max-md:visible relative" onClick={flagHandler}>
+         <div className='flex items-center'>Menu {!flag && <MdArrowDropDown className='mt-1 text-xl' />}
+         {flag && <MdArrowDropUp className='mt-1 text-xl' />}</div>
+         {flag && (<div className="flex-col justify-evenly [&>*]:flex mt-1 border-red-700 border-l-4 h-[5.5rem] max-sm:h-[4.5rem]
+          w-16 rounded-lg items-center [&>span]:cursor-pointer bg-black bg-opacity-90 font-light text-lg max-sm:text-base">
+          <Link to="/" className="hover:text-red-600 hover:font-normal ">
             Home
           </Link>
           <Link to="/about" className="hover:text-red-600 hover:font-normal">
@@ -68,7 +72,8 @@ const Header = () => {
           </Link>
           <div className="hover:text-red-600 hover:font-normal cursor-pointer relative group flex flex-col ">
             Tags
-            <ul className="w-60 h-[16rem]  bg-black text-white font-light [&>*]:my-2  [&>*]:px-4   absolute m-[2.5rem] py-4 px-3 invisible group-hover:visible hover:visible ">
+            <ul className="w-44 h-[13rem]  bg-black text-white font-light [&>*]:my-2 [&>*]:px-2 absolute mt-7 max-sm:mt-6
+            py-1 px-3 invisible group-hover:visible hover:visible text-base">
               <li
                 className="hover:animate-tagsBgChange"
                 onClick={() =>
@@ -162,8 +167,8 @@ const Header = () => {
             </ul>
           </div>
         </div>)}
-         
-         </div>         
+         </div> 
+
         <div className="max-md:invisible flex justify-evenly w-96 [&>*]:flex [&>*]:[&>*]:mt-1.5 mt-3 max-sm:mt-[0.6rem] 
         items-center  [&>span]:cursor-pointer font-light text-lg max-sm:text-[0.1rem] max-md:w-60  ">
           <Link to="/" className="hover:text-red-600 hover:font-normal">
@@ -174,7 +179,7 @@ const Header = () => {
           </Link>
           <div className="hover:text-red-600 hover:font-normal cursor-pointer relative group flex flex-col ">
             Tags
-            <ul className="w-60 h-[16rem]  bg-black text-white font-light [&>*]:my-2  [&>*]:px-4   absolute m-[2.5rem] py-4 px-3 invisible group-hover:visible hover:visible ">
+            <ul className="w-60 h-[16rem] bg-black text-white font-light [&>*]:my-2 [&>*]:px-4 absolute mx-[2.2rem] py-4 px-3 invisible group-hover:visible hover:visible ">
               <li
                 className="hover:animate-tagsBgChange"
                 onClick={() =>
